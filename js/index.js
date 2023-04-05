1e9b546240ccd99935b731cce332b20613b53a69
Kal2403 <kal2403199255@gmail.com>
2023-04-05 14:15:09 +0200

    test2 Form finish useing the DOM

--- js/index.js
+++ js/index.js
@@ -1,6 +1,10 @@
 const body = document.querySelector('body');
 const tbodyproducts = document.querySelector('#tbodyproducts')
 const btnAddUpdate = document.querySelector('#btn')
+const inProduct = document.querySelector('#inProduct')
+const inStock = document.querySelector('#inStock')
+const inPrice = document.querySelector('#inPrice')
+const inStatus = document.querySelector('#inState')
 
 body.onload = () => {
 
@@ -13,23 +17,45 @@ function fillTable(products){
     let trs = [];
 
     products.forEach((p, i) => {
-        const tr = createRow(p);
+        const tr = createRow(p, i);
         trs.push(tr)
     })
 
     tbodyproducts.append(...trs) 
 }
 
-function createRow(p){
+function clearTable(){
+    tbodyproducts.innerHTML = ''
+}
+
+function createRow(p, i){
     
     const iDelete = document.createElement('i');
     iDelete.className = 'bi bi-trash-fill';
     const tdDelete = document.createElement('td');
     tdDelete.appendChild(iDelete);
+    iDelete.addEventListener('click', () => {
+        const isconfirm = confirm('Estas Seguro de Eliminar')
+        if(isconfirm){
+            deleteProduct(i)
+            clearTable()
+            fillTable(getProducts())
+        }
+    })
 
     //icono de actualizar
     const iUpdate = document.createElement('i');
     iUpdate.className = 'bi bi-pen-fill';
+    iUpdate.addEventListener('click', () => {
+        inProduct.value = p.name
+        inStock.value = p.stock 
+        inPrice.value = p.price
+        inStatus.value = p.status
+        btnAddUpdate.textContent = 'Actualizar'
+
+        btnAddUpdate.onclick = (e) => handerUpdateClick(e, i)
+    })
+
     const tdUpdate = document.createElement('td');
     tdUpdate.appendChild(iUpdate);
 
@@ -64,23 +90,40 @@ function createRow(p){
 }
 
 
-btnAddUpdate.addEventListener('click', handerAddClick);
+btnAddUpdate.onclick = handerAddClick
 
 function handerAddClick(e){
-    const inProduct = document.querySelector('#inProduct');
-    const inStock = document.querySelector('#inStock')
-    const inPrice = document.querySelector('#inPrice')
-    const inStatus = document.querySelector('#inState')
-
     const valueInProduct = inProduct.value;
-    const valueInStock = parseInt(inStock.value);
-    const valueInPrice = parseFloat(inPrice.value);
-    const valueInStatus = parseInt(inStatus.value);
+    const valueInStock = parseInt(inStock.value)
+    const valueInPrice = parseFloat(inPrice.value)
+    const valueInStatus = parseInt(inStatus.value)
+    const i = getProducts().length
 
-    const newProduct = addProducto(valueInProduct, valueInStock, valueInPrice, valueInStatus)
-    const row = createRow(newProduct)
+    const newProduct = addProduct(valueInProduct, valueInStock, valueInPrice, valueInStatus)
+    const row = createRow(newProduct, i)
     tbodyproducts.appendChild(row)
-
+    alert('Producto Creado')
 
     e.preventDefault()
 }
+
+function handerUpdateClick(e, i){
+    const valueInProduct = inProduct.value;
+    const valueInStock = parseInt(inStock.value)
+    const valueInPrice = parseFloat(inPrice.value)
+    const valueInStatus = parseInt(inStatus.value)
+
+    updateProduct(i, valueInProduct, valueInStock, valueInPrice, valueInStatus)
+    clearTable()
+    const products = getProducts()
+    fillTable(products)
+    inProduct.value = ''
+    inStock.value = ''
+    inPrice.value = ''
+    inStatus.value = ''
+    btnAddUpdate.textContent = 'AGREGAR';
+    alert('Producto Actualizado')
+
+    e.preventDefault()
+}
+
